645205a4e49f9f05b1dc032c025fd2741b0d0d40
Jim Garrison <jim@garrison.cc>
2015-01-27 19:53:45 -0800

    Remove lgettext functions

--- src/Gettext.jl
+++ src/Gettext.jl
@@ -8,13 +8,9 @@ bindtextdomain(domain, localedir=nothing) = gt.bindtextdomain(domain, localedir)
 textdomain(domain=nothing) = gt.textdomain(domain)
 
 gettext(message) = gt.gettext(message)
-lgettext(message) = gt.lgettext(message)
 dgettext(domain, message) = gt.dgettext(domain, message)
-dlgettext(domain, message) = gt.dlgettext(domain, message)
 ngettext(singular, plural, n) = gt.ngettext(singular, plural, n)
-lngettext(singular, plural, n) = gt.lngettext(singular, plural, n)
 dngettext(domain, singular, plural, n) = gt.dngettext(domain, singular, plural, n)
-ldngettext(domain, singular, plural, n) = gt.ldngettext(domain, singular, plural, n)
 
 macro __str(s)
     gettext(s)
